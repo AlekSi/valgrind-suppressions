4714737095ee3ff6f3b8072a77a2497713ff3c58
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2010-01-17 02:45:01 +0300

    Reuse QSqlQuery in Qt4.

--- 50-Qt4/54-Sql/main.cpp
+++ 50-Qt4/54-Sql/main.cpp
@@ -13,22 +13,21 @@ int main(int argc, char *argv[])
 		db.setDatabaseName(":memory:");
 		db.open();
 		db.transaction();
-		QSqlQuery q1("CREATE TABLE t (i INTEGER)", db);
-		q1.prepare("INSERT INTO t (i) VALUES(?)");
-		q1.addBindValue(42);
-		q1.exec();
+		QSqlQuery q("CREATE TABLE t (i INTEGER)", db);
+		q.prepare("INSERT INTO t (i) VALUES(?)");
+		q.addBindValue(42);
+		q.exec();
 		db.commit();
 
 		db.transaction();
-		QSqlQuery q2(db);
-		q2.setForwardOnly(true);
-		q2.setForwardOnly(false);
-		q2.exec("SELECT * FROM t");
-		q2.first();
-		int value = q2.value(0).toInt();
+		q.setForwardOnly(true);
+		q.setForwardOnly(false);
+		q.exec("SELECT * FROM t");
+		q.first();
+		int value = q.value(0).toInt();
 		Q_ASSERT(value == 42);
-		q2.last();
-		q2.finish();
+		q.last();
+		q.finish();
 	}
 	QSqlDatabase::removeDatabase("connection_name");
 
