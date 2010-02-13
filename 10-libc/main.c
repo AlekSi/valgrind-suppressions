cee8bf3a8b8c9ff4c320d2700efc447eb29f213e
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2010-02-13 18:39:18 +0300

    Added printf and atexit.

--- 10-libc/main.c
+++ 10-libc/main.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+void do_atexit()
+{
+}
 
 int main()
 {
+	atexit(do_atexit);
 	atoi("42");
 	atof("42.42");
+	printf("printf: %d %s\n", 42, "some string");
 	return 0;
 }
